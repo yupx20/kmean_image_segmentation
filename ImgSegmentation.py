3ea7cfdbfd8df1729027bdbee919cc50fbe61379
yupx20 <muhammad.rizal.yudianto-2021@fst.unair.ac.id>
2025-02-27 10:59:35 +0700

    delete notes in code

--- ImgSegmentation.py
+++ ImgSegmentation.py
@@ -3,24 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-# Read image file
 script_dir = os.path.dirname(os.path.abspath(__file__))
 image_filename = "test.jpg"
 image_path = os.path.join(script_dir, image_filename)
 image = cv2.imread(image_path)
 
-# Convert the color into RGB dan LAB
 imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 imageLAB = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 
-# Image pre-processing
 pixel_values = imageLAB.reshape((-1, 3))
 pixel_values = np.float32(pixel_values)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
-# K-Means image segmentation
-k = 7 # Adjust the k value for the best segmentation
+k = 7
 _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 centers = np.uint8(centers)
@@ -28,15 +24,12 @@ labels = labels.flatten()
 segmented_image = centers[labels]
 segmented_image = segmented_image.reshape(imageLAB.shape)
 
-# Saving segmented image to BGR format
 segmented_image_bgr = cv2.cvtColor(segmented_image, cv2.COLOR_LAB2BGR)
 output_filename = "segmented.jpg"
 output_path = os.path.join(script_dir, output_filename)
 cv2.imwrite(output_path, segmented_image_bgr)
 print(f"Segmented image saved as {output_path}")
 
-
-# Display the original and segmented image
 plt.subplot(211), plt.imshow(imageRGB)
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(212), plt.imshow(segmented_image)
